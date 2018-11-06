44dff34eec84de57907ff21cd82996e48d6400f3
Adam Duff <j.adam.duff@gmail.com>
2018-11-06 19:06:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ $(document).ready(function (){
       console.log(response);
       let html = '<ul>';
       for (const repo of response['items']) {
-        html += '<p><ul><li>Repo Name: <a href="' + repo['html_url'] + '">' + repo['name'] + '</a></li><li>' + repo['description'] + '</li><li>Owner Login' + repo['owner']['login'] + '</li><li><img src="' + repo['owner']['avatar_url'] + '" /><li><a href="' + repo['owner']['html_url'] + '">Profile</a></li><li><a href="#" data-repository="' + repo['name'] + '" data-owner="' + repo['owner']['login'] + '" onclick="showCommits(this)">Show Commits</a></li></ul></p>';
+        html += '<p><ul><li>Repo Name: <a href="' + repo['html_url'] + '">' + repo['name'] + '</a></li><li>' + repo['description'] + '</li><li>Owner Login' + repo['owner']['login'] + '</li><li><img src="' + repo['owner']['avatar_url'] + '" width="50px" /><li><a href="' + repo['owner']['html_url'] + '">Profile</a></li><li><a href="#" data-repository="' + repo['name'] + '" data-owner="' + repo['owner']['login'] + '" onclick="showCommits(this)">Show Commits</a></li></ul></p>';
       }
       html += '</ul>';
       $('#results').html(html);
@@ -29,5 +29,10 @@ let showCommits = function(el) {
   let url = 'https://api.github.com/repos/' + el.dataset.owner + '/' + el.dataset.repository + '/commits';
   $.get(url, function(response) {
     console.log(response);
+    let html = '<ul>';
+    for (const repo of response['items']) {
+      html += '<p><ul><li>SHA: ' + repo['sha'] + '</li><li></li></ul></p>'
+    }
+    html += '</ul>';
   });
 }
