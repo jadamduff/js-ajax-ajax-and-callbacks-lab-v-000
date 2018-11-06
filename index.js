de005650036c3f49726df6c84e2148b872b40070
Adam Duff <j.adam.duff@gmail.com>
2018-11-06 18:44:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ $(document).ready(function (){
     $.get(url, function(response) {
       console.log(response);
       let html = '<ul>';
-      for (const repo of response) {
+      for (const repo of response['items']) {
         html += '<ul><li>Repo Name: <a href="' + repo['html_url'] + '">' + repo['name'] + '</a></li><li>' + repo['description'] + '</li><li>Owner Login' + repo['owner']['login'] + '</li><li><img src="' + repo['owner']['avatar_url'] + '" /><li><a href="' + repo['owner']['html_url'] + '">Profile</a></li></ul>';
       }
       $('#results').html(html);
