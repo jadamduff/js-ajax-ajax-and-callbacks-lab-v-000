4d8d54da47d3a2c19f2b46114827ea6d4298d67b
Adam Duff <j.adam.duff@gmail.com>
2018-11-06 18:28:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 $(document).ready(function (){
   let searchRepositories = function() {
-    console.log('woerkse');
+    let url = 'https://api.github.com/search/repositories?q=';
+    let terms = $('#searchTerms').split(' ');
+    for (const term of terms) {
+      if (indexOf(term) === 0) {
+        url += term;
+      } else {
+        url += '+' + term;
+      }
+    }
+
+    $.get(url, function(response) {
+      console.log(response);
+    })
   };
 
   $('#searchBtn').on('click', searchRepositories);
