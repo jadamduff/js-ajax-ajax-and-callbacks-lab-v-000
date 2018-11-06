669290a86b94a942a5b3d5fede307b73cb4856fe
Adam Duff <j.adam.duff@gmail.com>
2018-11-06 18:47:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ $(document).ready(function (){
   let searchRepositories = function() {
     let url = 'https://api.github.com/search/repositories?q=';
     let terms = $('#searchTerms').val().split(' ');
+
     for (const term of terms) {
       if (terms.indexOf(term) === 0) {
         url += term;
@@ -14,11 +15,15 @@ $(document).ready(function (){
       console.log(response);
       let html = '<ul>';
       for (const repo of response['items']) {
-        html += '<ul><li>Repo Name: <a href="' + repo['html_url'] + '">' + repo['name'] + '</a></li><li>' + repo['description'] + '</li><li>Owner Login' + repo['owner']['login'] + '</li><li><img src="' + repo['owner']['avatar_url'] + '" /><li><a href="' + repo['owner']['html_url'] + '">Profile</a></li><li><a href="#" onclick="showCommits">Show Commits</a></li></ul>';
+        html += '<ul><li>Repo Name: <a href="' + repo['html_url'] + '">' + repo['name'] + '</a></li><li>' + repo['description'] + '</li><li>Owner Login' + repo['owner']['login'] + '</li><li><img src="' + repo['owner']['avatar_url'] + '" /><li><a href="' + repo['owner']['html_url'] + '">Profile</a></li><li><a href="#" onclick="showCommits(this)">Show Commits</a></li></ul>';
       }
       $('#results').html(html);
     });
   };
+
+  let showCommits = function() {
+
+  }
 
   $('#searchBtn').on('click', searchRepositories);
 });
